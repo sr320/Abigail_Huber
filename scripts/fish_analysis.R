7e3aee1279320f1861a976c57f0fc6037b901b51
ahuber3 <74076345+ahuber3@users.noreply.github.com>
2020-11-10 10:30:04 -0800

    Mutated

--- scripts/fish_analysis.R
+++ scripts/fish_analysis.R
@@ -4,3 +4,6 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data.csv")
 library(dplyr)
 fish_data_cat <- fish_data %>%
   mutate(length_cat = ifelse(length > 200, "big", "small"))
+
+fish_data_cat <- fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
